a67ebcaa216a966d3cacd939a0c1238afe9a314c
Sam Bolgert <sbolgert@gmail.com>
2011-11-22 15:29:29 -0600

    idk

--- src/edu/uwsp/lucene/LuceneMain.java
+++ src/edu/uwsp/lucene/LuceneMain.java
@@ -1,7 +1,9 @@
 package edu.uwsp.lucene;
 
-import org.apache.lucene.document.Document;
-
+//import org.apache.lucene.document.Document;
+import org.w3c.*; 
+import org.w3c.dom.Document;
+import org.apache.lucene.document.*;
 
 
 
@@ -10,13 +12,22 @@ public class LuceneMain {
 	/**
 	 * @param args
 	 */
+	private static org.apache.lucene.document.Document luceneDocument;
+	
 	public static void main(String[] args) {
 		String dirPath = "/data/";
 		String fileName = "address-example.xml";
 		
-		Document xmlDoc = DOMUtil.parse();
-		xmlDoc.getChildNodes().item(0).
-
+		//parses XML file into Document object
+		Document xmlDoc = DOMUtil.parse(dirPath+fileName);
+	
+		luceneDocument = new org.apache.lucene.document.Document();
+		//luceneDocument.add(new Field.Text());
+		
+	
+		
 	}
+	
+	
 
 }
