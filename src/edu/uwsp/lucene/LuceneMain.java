126b8d62f4e482971d92b013d24ed53b5360e387
Sam Bolgert <sbolgert@gmail.com>
2011-11-22 15:03:49 -0600

    testing stuff

--- src/edu/uwsp/lucene/LuceneMain.java
+++ src/edu/uwsp/lucene/LuceneMain.java
@@ -1,12 +1,21 @@
 package edu.uwsp.lucene;
 
+import org.apache.lucene.document.Document;
+
+
+
+
 public class LuceneMain {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String dirPath = "/data/";
+		String fileName = "address-example.xml";
+		
+		Document xmlDoc = DOMUtil.parse();
+		xmlDoc.getChildNodes().item(0).
 
 	}
 
